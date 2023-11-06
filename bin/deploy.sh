ec00d5bbb1f005c97052cf2dc9569a59fae26917
Narumi Abe <mail.narumi@gmail.com>
2023-11-06 12:41:57 -0300

    added MANIFEST.in

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,3 +1,4 @@
 cd ..
-python3 setup.py sdist
-python3 -m twine upload dist/*
+python3 -m build
+#python3 setup.py sdist
+python3 -m twine upload dist/* --verbose
--- setup.py
+++ setup.py
@@ -2,8 +2,9 @@ from setuptools import setup, find_packages
 
 setup(
     name="cannoli",
-    version="0.1.2",
+    version="0.1.4",
     packages=find_packages(),
+    include_packages_data=True,
     install_requires=[
         'openai',
         'pandas',
